57cc78f44d1537659039d660eebd8527a6609bd9
keli06001 <keli06001@E-PW0234HP>
2023-09-06 10:26:10 +0200

    Add project files.

---
+++ Uppgift1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; //namespace gör det lättare att sortera
+
+namespace uppgift1_1 //class program ger nästkommande kommandon egenskaper
+{
+    class program //Static Void Main gör så att koden inom inte påvärkar andra koder
+    {
+        static void Main(string[] args) //nästkommande kommando gör så att text kommer upp i programet
+        {
+                Console.WriteLine("Du är nästan klar med kapitel 1."); //nästa kommando är likadant
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\"); //nästa tecken avslutar Static Void Main
+        }
+    }
+}  
